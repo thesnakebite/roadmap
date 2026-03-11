12b3c90b3436ac94397166f846ac6a56b4eaa34c
Álvaro Navarro <alvaro@thesnakebite.es>
2026-03-11 16:57:26 +0100

    feat: add custom Filament theme and reorganize feature form into tabs with JS-driven styling

--- app/Filament/Resources/Features/Schemas/FeatureForm.php
+++ app/Filament/Resources/Features/Schemas/FeatureForm.php
@@ -5,13 +5,14 @@ namespace App\Filament\Resources\Features\Schemas;
 use App\Enums\Feature\FeatureStatus;
 use App\Enums\Feature\FeatureType;
 use Filament\Forms\Components\DatePicker;
-//  use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Slider;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\ToggleButtons;
+use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
 use Illuminate\Validation\Rule;
@@ -23,82 +24,94 @@ class FeatureForm
         return $schema
             ->columns(3)
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                Select::make('status')
-                    ->options(FeatureStatus::class)
-                    ->enum(FeatureStatus::class)
-                    ->searchable()
-                    ->required()
-                    ->default(FeatureStatus::Proposed),
-                DatePicker::make('target_delivery_date')
-                    ->rules([
-                        function (Get $get) {
-                            return Rule::requiredIf($get('status') === FeatureStatus::Planned || $get('status') === FeatureStatus::InProgress);
-                        },
-                    ])
-                    ->required()
-                    ->visibleJs(<<<'JS'
-                        $get('status') === 'Planned' || $get('status') === 'In Progress'
-                    JS
-                    ),
-                ToggleButtons::make('type')
-                    ->hiddenLabel()
-                    ->options(FeatureType::class)
-                    ->enum(FeatureType::class)
-                    ->inline()
-                    ->required()
-                    ->default(FeatureType::Feature),
-                Slider::make('priority')
-                    ->minValue(1)
-                    ->maxValue(10)
-                    ->pips(Slider\Enums\PipsMode::Steps)
-                    ->step(1)
-                    ->fillTrack()
-                    ->required()
-                    ->default(0),
-                RichEditor::make('description')
+                Tabs::make('Feature Tabs')
                     ->columnSpanFull()
-                    ->required(),
-                TextInput::make('effort_in_days')
-                    ->required()
-                    ->numeric()
-                    ->afterStateUpdatedJs(<<<'JS'
-                        const isHighCost = $get('is_high_cost');
-                        const effort = $state;
-                        const costPerDay = isHighCost ? 1500 : 1000;
-                        $set('cost', effort * costPerDay);
-                    JS
-                    )
-                    // ->live(onBlur: true)
-                    // ->afterStateUpdated(function ($state, Set $set, Get $get) {
-                    //     $set('cost', $get('is_high_cost') ? 1500 * $state : 1000 * $state);
-                    // })
-                    ->default(0),
-                TextInput::make('cost')
-                    ->required()
-                    ->numeric()
-                    ->default(0.0)
-                    ->prefix('$'),
-                Toggle::make('is_high_cost')
-                    ->label('Is High Cost')
-                    ->dehydrated(false)
-                    ->afterStateUpdatedJs(<<<'JS'
-                        const isHighCost = $state;
-                        const effort = $get('effort_in_days');
-                        const costPerDay = isHighCost ? 1500 : 1000;
-                        $set('cost', effort * costPerDay);
-                    JS
-                    ),
-                // ->live()
-                // ->afterStateUpdated(function ($state, Set $set, Get $get) {
-                //     if ($state) {
-                //         $set('cost', $get('effort_in_days') * 1500);
-                //     } else {
-                //         $set('cost', $get('effort_in_days') * 1000);
-                //     }
-                // })
-                // DateTimePicker::make('delivered_at'),
+                    ->tabs([
+                        Tabs\Tab::make('General')
+                            ->schema([
+                                TextInput::make('name')
+                                    ->required(),
+                                Select::make('status')
+                                    ->options(FeatureStatus::class)
+                                    ->enum(FeatureStatus::class)
+                                    ->searchable()
+                                    ->required()
+                                    ->default(FeatureStatus::Proposed),
+                                Slider::make('priority')
+                                    ->extraFieldWrapperAttributes([
+                                        'class' => 'pl-3',
+                                    ])
+                                    ->minValue(1)
+                                    ->maxValue(10)
+                                    ->pips(Slider\Enums\PipsMode::Steps)
+                                    ->step(1)
+                                    ->fillTrack()
+                                    ->required()
+                                    ->default(0),
+                                ToggleButtons::make('type')
+                                    ->hiddenLabel()
+                                    ->options(FeatureType::class)
+                                    ->enum(FeatureType::class)
+                                    ->inline()
+                                    ->required()
+                                    ->default(FeatureType::Feature),
+                                DatePicker::make('target_delivery_date')
+                                    ->rules([
+                                        function (Get $get) {
+                                            return Rule::requiredIf($get('status') === FeatureStatus::Planned || $get('status') === FeatureStatus::InProgress);
+                                        },
+                                    ])
+                                    ->required()
+                                    ->visibleJs(
+                                        <<<'JS'
+                                            $get('status') === 'Planned' || $get('status') === 'In Progress'
+                                        JS
+                                    ),
+                                DateTimePicker::make('delivered_at')
+                                    ->visibleJs(
+                                        <<<'JS'
+                                        $get('status') === 'Completed'
+                                    JS
+                                    ),
+                                RichEditor::make('description')
+                                    ->extraInputAttributes([
+                                        'style' => 'min-height: 150px;',
+                                    ])
+                                    ->columnSpanFull()
+                                    ->required(),
+                            ]),
+
+                        Tabs\Tab::make('Effort and Cost')
+                            ->schema([
+                                TextInput::make('effort_in_days')
+                                    ->required()
+                                    ->numeric()
+                                    ->afterStateUpdatedJs(<<<'JS'
+                                                const isHighCost = $get('is_high_cost');
+                                                const effort = $state;
+                                                const costPerDay = isHighCost ? 1500 : 1000;
+                                                $set('cost', effort * costPerDay);
+                                            JS
+                                    )
+                                    ->default(0),
+                                TextInput::make('cost')
+                                    ->required()
+                                    ->numeric()
+                                    ->default(0.0)
+                                    ->prefix('$'),
+                                Toggle::make('is_high_cost')
+                                    ->label('Is High Cost')
+                                    ->dehydrated(false)
+                                    ->afterStateUpdatedJs(
+                                        <<<'JS'
+                                            const isHighCost = $state;
+                                            const effort = $get('effort_in_days');
+                                            const costPerDay = isHighCost ? 1500 : 1000;
+                                            $set('cost', effort * costPerDay);
+                                        JS
+                                    ),
+                            ]),
+                    ]),
             ]);
     }
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->unsavedChangesAlerts(fn () => app()->isProduction())
             ->databaseTransactions()
             ->strictAuthorization()
